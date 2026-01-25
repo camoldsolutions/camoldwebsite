6c0aa9a811d451d9a28aa427f1a23a82432708e5
rarebirdgames <austins@rarebirdgames.com>
2026-01-24 16:24:58 -0800

    Feature: Replace About page image with mold inspection photos

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,8 @@ export const ASSETS = {
     logo: "/Logo_Alpha.png",
     heroImage: "https://images.squarespace-cdn.com/content/v1/695e124922ee964abeffdd7f/3afe0f26-ea59-467c-8422-779a5c0b9cb0/MOD+4.PNG",
     vanImage: "https://images.squarespace-cdn.com/content/v1/695e124922ee964abeffdd7f/482b5316-12f4-415a-9d94-dc5f845d4c73/IMG_3311.jpg",
-    moldImage: "https://images.squarespace-cdn.com/content/v1/695e124922ee964abeffdd7f/2315047b-7242-47c8-ab08-4a8620cbe4f9/mold.png",
+    moldImage: "/moldinspection.png",
+    moldImage2: "/moldinspection_02.png",
     inspectionImage: "https://images.unsplash.com/photo-1581094794329-c8112a89af12?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80",
     equipmentImage: "https://images.unsplash.com/photo-1581092795360-f9814eb1b60c?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80"
 };
